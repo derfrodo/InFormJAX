598699655c12ad6b1fd182c0016c868aaf3bf690
Stefan Pauka <14931959+derfrodo@users.noreply.github.com>
2023-04-29 22:57:35 +0200

    radius and apollo server

--- code/demo/webapp/src/pages/_app.tsx
+++ code/demo/webapp/src/pages/_app.tsx
@@ -1,7 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 import "@/Wheel/App.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: "/api/graphql",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }

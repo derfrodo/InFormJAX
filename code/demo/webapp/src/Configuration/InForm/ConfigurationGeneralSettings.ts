c51c269236a3dc64901f19cb04da9397c25591c0
Stefan Pauka <14931959+derfrodo@users.noreply.github.com>
2023-05-02 14:38:52 +0200

    Config cleaned up

--- code/demo/webapp/src/Configuration/InForm/ConfigurationGeneralSettings.ts
+++ code/demo/webapp/src/Configuration/InForm/ConfigurationGeneralSettings.ts
@@ -38,13 +38,6 @@ interface FM extends InFormDataTypes<string, string> {
     typeof IntInput
   >;
 
-  ["STRINGSCALAR"]: InFormDataTypeWithDefaultValueAndComponent<
-    "STRING",
-    Scalars["String"],
-    "",
-    typeof StringInput
-  >;
-
   ["StNullUndef"]: InFormDataTypeWithDefaultValueAndComponent<
     "STRING",
     string | null,
@@ -99,23 +92,6 @@ interface FM extends InFormDataTypes<string, string> {
     false,
     typeof BoolInput
   >;
-
-  // Specific properties (resolve them using RegEx)
-  // ["identityCardExpires"]: InFormDataTypeWithDefaultValueAndComponent<"ID", Scalars['DateTime'], "2022-11-09T00:00:00Z", typeof StringInput,
-  // InFormPropertyMatcherRegexRegex<typeof identityCardExpires>>
-
-  // // General Properties with value types
-  // ["STRING"]: InFormDataTypeWithDefaultValueAndComponent<"STRING", Scalars['String'], "", typeof FakeComp>
-  // ["INTEGER"]: InFormDataTypeWithDefaultValueAndComponent<"INTEGER", Scalars['Int'], 0, typeof FakeComp>
-  // ["FLOAT"]: InFormDataTypeWithDefaultValueAndComponent<"FLOAT", Scalars['Float'], 0, typeof FakeComp>
-  // ["MAYBEFLOAT"]: InFormDataTypeWithDefaultValueAndComponent<"MAYBEFLOAT", InputMaybe<Scalars['Float']>, null, typeof FakeComp2>
-
-  // // General Properties with value types, but logic due to representing special stuff (dates ;) )
-  // ["DATE"]: InFormDataTypeWithDefaultValueAndComponent<"DATE", Scalars['DateTime'], null, typeof FakeComp>
-  // ["MAYBEDATE"]: InFormDataTypeWithDefaultValueAndComponent<"MAYBEDATE", InputMaybe<Scalars['DateTime']>, null, typeof FakeComp>
-
-  // // Select fields - currently implemented in a specific way
-  // ["Genders"]: InFormDataTypeWithDefaultValueAndComponent<"Genders", Genders, Genders.None, typeof FakeComp>
 }
 
 export interface ConfigurationGeneralSettings

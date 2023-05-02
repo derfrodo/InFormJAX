9c6c53f8b98acf07cb989bbe63bc483885b0e266
Stefan Pauka <14931959+derfrodo@users.noreply.github.com>
2023-05-02 17:07:51 +0200

    animate pending wheel

--- code/demo/webapp/src/api/data/sessionWheelSettings.ts
+++ code/demo/webapp/src/api/data/sessionWheelSettings.ts
@@ -6,6 +6,6 @@ import { SPIN_INNER_DURATION } from "../../Wheel/constants/SPIN_INNER_DURATION";
 export const sessionWheelSettings: UpdateWheelSettingsMutationReturnType = {
   radius: RADIUS,
   rotationDurationInner: SPIN_INNER_DURATION,
-  rotationDurationNotPlaying: Math.floor(SPIN_DURATION / 100),
+  rotationDurationNotPlaying: Math.floor(SPIN_DURATION * 100),
   rotationDurationPlaying: SPIN_DURATION,
 };

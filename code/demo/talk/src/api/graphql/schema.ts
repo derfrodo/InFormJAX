0457618feb0195928c0bb6a74cf25bb4fe1163d7
Stefan Pauka <14931959+derfrodo@users.noreply.github.com>
2023-05-04 16:51:40 +0200

    code for talk

--- code/demo/talk/src/api/graphql/schema.ts
+++ code/demo/talk/src/api/graphql/schema.ts
@@ -4,18 +4,21 @@ import {
 } from "graphql";
 
 import { userMutations, userQueries } from "./types/User/userSchema";
+import { contactMutations, contactQueries } from "./types/Contact/contactSchema";
 
 export const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: "Query",
     fields: {
-      ...userQueries
+      ...userQueries,
+      ...contactQueries
     },
   }),
   mutation: new GraphQLObjectType({
     name: "Mutation",
     fields: {
       ...userMutations,
+      ...contactMutations
     },
   }),
 });

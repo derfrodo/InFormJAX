5c14e27d09f1cac6b3340859135ae4ba7a739110
Stefan Pauka <14931959+derfrodo@users.noreply.github.com>
2023-05-07 12:34:42 +0200

    forward if needed

--- code/demo/webapp/src/pages/_app.tsx
+++ code/demo/webapp/src/pages/_app.tsx
@@ -3,10 +3,21 @@ import { getClient } from "@/gql/getApolloClient";
 import "@/styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
-
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const client = getClient(pageProps);
+
+  useEffect(() => {
+    if (
+      location &&
+      location.href.indexOf(":3000") > 0 &&
+      location.href.indexOf("localhost") > 0
+    ) {
+      location.href = "http://127.0.0.1:3000";
+    }
+  }, []);
 
   return (
     <ApolloProvider client={client}>
